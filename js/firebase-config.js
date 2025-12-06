79df285f462b17891ec36aca40858aa9c33f3b91
Stuart-McNeill <84739707+Stuart-McNeill@users.noreply.github.com>
2025-12-06 13:51:35 +0000

    Add Firestore initialization to firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,8 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-export const firebaseConfig = {
+export const app = initializeApp({
   apiKey: "AIzaSyDpERVmyx4n6t1yW7YxHPKVsfmY4bOVBv0",
   authDomain: "html-website-auth-v2.firebaseapp.com",
   projectId: "html-website-auth-v2",
@@ -9,6 +10,7 @@ export const firebaseConfig = {
   messagingSenderId: "789981394325",
   appId: "1:789981394325:web:f012a35a12d3b49584f29c",
   measurementId: "G-HNCGD9YYG2"
-};
+});
 
-export const app = initializeApp(firebaseConfig);
+// THIS MUST EXIST so Firestore is registered in Firebase
+export const db = getFirestore(app);
