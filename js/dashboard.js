b0b5ee2773eb11ad45b8827e3b2403855e492f2b
Stuart-McNeill <84739707+Stuart-McNeill@users.noreply.github.com>
2025-12-06 14:33:58 +0000

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,40 +1,24 @@
-//dashboard.js
-
+// dashboard.js
 import { app } from "./firebase-config.js";
-import {
+import { 
   getAuth,
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-import {
-  getFirestore,
-  doc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-const nameLabel = document.getElementById("namelabel");
-
-onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "../index.html";
     return;
   }
 
-  const userRef = doc(db, "users", user.uid);
-  const snapshot = await getDoc(userRef);
-
-  if (snapshot.exists()) {
-    const userData = snapshot.data();
-    nameLabel.textContent = userData.fullName;
-  } else {
-    nameLabel.textContent = user.email;
-  }
+  document.getElementById("namelabel").textContent = user.email;
 });
 
 document.getElementById("logoutbtn").addEventListener("click", () => {
-  signOut(auth).then(() => window.location.href = "../index.html");
+  signOut(auth).then(() => {
+    window.location.href = "../index.html";
+  });
 });
